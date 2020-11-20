8144d245e1f3f318b8e4513a660a4bb11bba14c0
olegsey <71653455+Oleg11713@users.noreply.github.com>
2020-11-20 13:37:03 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,120 +8,160 @@ using namespace std;
 class Administrirovanie
 {
 protected:
-	int age, zp;
-	string name;
-	string surname;
-	string firma;
+	string company;
+	string position;
 public:
-	Administrirovanie(int n, int zp1, string name1, string surname1, string firma1)
+	Administrirovanie(string company1, string position1)
 	{
-		age = n;
-		zp = zp1;
-		name = name1;
-		surname = surname1;
-		firma = firma1;
+		company = company1;
+		position = position1;
 	}
-	virtual void show() = 0;
-	virtual void Pension(int n, string gender) = 0;
+	Administrirovanie() {};
+	void show()
+	{
+		cout << "Company: " << company << endl;
+		cout << "Position: " << position << endl;
+	}
+	void changeCompany(string ptr)
+	{
+		company = ptr;
+	}
+	void changePosition(string ptr)
+	{
+		position = ptr;
+	}
+	string getCompany()
+	{
+		return company;
+	}
+	string getPosition()
+	{
+		return position;
+	}
 };
 
-class Director : virtual public Administrirovanie
+class WorkersCharacters : virtual public Administrirovanie
 {
 protected:
-	string gender;
-	string obrazovanie;
+	int expirience;
+	string surname;
+	string name;
 public:
-	Director(int n, int zp1, string name1, string surname1, string firma1, string gender1, string obrazovanie1) :Administrirovanie(n, zp1, name1, surname1, firma1)
+	WorkersCharacters(string company1, string position1, int expirience1, string surname1, string name1) :Administrirovanie(company1, position1)
 	{
-		obrazovanie = obrazovanie1;
+		expirience = expirience1;
+		surname = surname1;
+		name = name1;
+	}
+	WorkersCharacters() 
+	{
+		expirience = 0;
+	};
+	void show()
+	{
+		cout << "Surname: " << surname << endl;
+		cout << "Name: " << name << endl;
+		cout << "Expirience (how many years): " << expirience << endl;
+	}
+	void changeExpirience(int ptr)
+	{
+		expirience = ptr;
+	}
+	void changeSurname(string ptr)
+	{
+		surname = ptr;
+	}
+	void changeName(string ptr)
+	{
+		name = ptr;
+	}
+	int getExpirience()
+	{
+		return expirience;
+	}
+	string getSurname()
+	{
+		return surname;
+	}
+	string getName()
+	{
+		return name;
+	}
+};
+
+class PersonalCharacters : virtual public Administrirovanie
+{
+protected:
+	int age;
+	string gender;
+public:
+	PersonalCharacters(string company1, string position1, int age1, string gender1) :Administrirovanie(company1, position1)
+	{
+		age = age1;
 		gender = gender1;
 	}
+	PersonalCharacters() 
+	{
+		age = 0;
+	};
 	void show()
 	{
-		cout << "-----------Director-----------" << endl;
-		cout << surname << " " << name << " " << age << " years old";
-		cout << "\nCompany: " << firma;
-		cout << "\nSalary: " << zp << "$";
-		cout << "\nGender: " << gender;
-		cout << "\nEducation: " << obrazovanie << endl << endl;
-	};
-	void Pension(int n, string gender)
+		cout << age << " years old" << endl;
+		cout << "Gender: " << gender << endl;
+	}
+	void changeAge(int ptr)
 	{
-		if (gender == "male" || gender == "Male")
-			if (n < 62)
-				cout << 62 - n << " years left until retirement" << endl << endl;
-			else cout << "May already retire" << endl << endl;
-		else if (gender == "female" || gender == "Female")
-			if (n < 57)
-				cout << 57 - n << " years left until retirement" << endl << endl;
-			else cout << "May already retire" << endl << endl;
-		else cout << "Incorrect gender entered for determining the date of retirement" << endl << endl;
+		age = ptr;
+	}
+	void changeGender(string ptr)
+	{
+		gender = ptr;
+	}
+	int getAge()
+	{
+		return age;
+	}
+	string getGender()
+	{
+		return gender;
 	}
 };
 
-class productManager : virtual public Administrirovanie
+class Human : public WorkersCharacters, public PersonalCharacters
 {
-protected:
-	int stazh;
-	string type;
+	string education;
+	string status;
 public:
-	productManager(int n, int zp1, string name1, string surname1, string firma1, int l, string type1) :Administrirovanie(n, zp1, name1, surname1, firma1)
+	Human(string company1, string position1, int expirience1, string surname1, string name1, int age1, string gender1, string education1, string status1)
+		:WorkersCharacters(company1, position1, expirience1, surname1, name1), PersonalCharacters(company1, position1, age1, gender1)
 	{
-		stazh = l;
-		type = type1;
+		education = education1;
+		status = status1;
 	}
-	void show()
+	Human() {};
+	void showHumanInfo()
 	{
-		cout << "-----------Manager-----------" << endl;
-		cout << surname << " " << name << " " << age << " years old";
-		cout << "\nCompany: " << firma;
-		cout << "\nSalary: " << zp << "$";
-		cout << "\nExpirience: " << stazh << " year(s)";
-		cout << "\nType: " << type << endl << endl;
-	};
-	void Pension(int n, string gender)
-	{
-		if (n < 62)
-			cout << 62 - n << " years left until retirement" << endl << endl;
-		else cout << "May already retire" << endl << endl;
+		WorkersCharacters::show();
+		Administrirovanie::show();
+		PersonalCharacters::show();
+		cout << "Education: " << education << endl;
+		cout << "Status: " << status << endl;
 	}
-};
-
-class Programmer : public Director, public productManager
-{
-	string role;
-	string kind;
-public:
-	Programmer(int n, int zp1, string name1, string surname1, string firma1, string kind1, string role1, string gender1, string obrazovanie1, int l, string type1)
-	:Administrirovanie(n, zp1, name1, surname1, firma1), Director(n, zp1, name1, surname1, firma1, gender1, obrazovanie1), productManager(n, zp1, name1, surname1, firma1, l, type1)
+	void changeEducation(string ptr)
 	{
-		kind = kind1;
-		role = role1;
+		education = ptr;
 	}
-	void show()
+	void changeStatus(string ptr)
 	{
-		cout << "-----------Programmer-----------" << endl;
-		cout << surname << " " << name << " " << age << " years old";
-		cout << "\nCompany: " << firma;
-		cout << "\nSalary: " << zp << "$";
-		cout << "\nRole: " << role;
-		cout << "\nProgrammer's kind: " << kind;
-		cout << "\nGender: " << gender;
-		cout << "\nEducation: " << obrazovanie;
-		cout << "\nExpirience: " << stazh << " year(s)";
-		cout << "\nType: " << type << endl << endl;
-	};
-	void Pension(int n, string gender)
+		status = ptr;
+	}
+	string getEducation()
 	{
-		if (gender == "male" || gender == "Male")
-			if (n < 62)
-				cout << 62 - n << " years left until retirement" << endl << endl;
-			else cout << "May already retire" << endl << endl;
-		else if (gender == "female" || gender == "Female")
-			if (n < 57)
-				cout << 57 - n << " years left until retirement" << endl << endl;
-			else cout << "May already retire" << endl << endl;
-		else cout << "Incorrect gender entered for determining the date of retirement" << endl << endl;
+		return education;
+	}
+	string getStatus()
+	{
+		return status;
 	}
 };
 
@@ -188,45 +228,16 @@ int menuBegin()
 	system("cls");
 	while (true)
 	{
-		choiceButton = (choiceButton + 3) % 3;
+		choiceButton = (choiceButton + 4) % 4;
 
 		if (choiceButton == 0) cout << "-> Write information" << endl;
 		else cout << " Write information" << endl;
 
-		if (choiceButton == 1) cout << "-> Display all information" << endl;
-		else cout << " Display all information" << endl;
+		if (choiceButton == 1) cout << "-> Correct information" << endl;
+		else cout << " Correct information" << endl;
 
-		if (choiceButton == 2) cout << "-> Exit" << endl;
-		else cout << " Exit" << endl;
-
-		button = _getch();
-		if (button == 224)
-		{
-			button = _getch();
-			if (button == 72) choiceButton--;
-			if (button == 80) choiceButton++;
-		}
-		if (button == 13) { system("cls"); return choiceButton; }
-		system("cls");
-	}
-}
-
-int menuWrite()
-{
-	int choiceButton = 0, button = 0;
-	system("cls");
-	while (true)
-	{
-		choiceButton = (choiceButton + 4) % 4;
-
-		if (choiceButton == 0) cout << "-> Write information about Director" << endl;
-		else cout << " Write information about Director" << endl;
-
-		if (choiceButton == 1) cout << "-> Write information about Manager" << endl;
-		else cout << " Write information about Manager" << endl;
-
-		if (choiceButton == 2) cout << "-> Write information about Programmer" << endl;
-		else cout << " Write information about Programmer" << endl;
+		if (choiceButton == 2) cout << "-> Display information" << endl;
+		else cout << " Display all information" << endl;		
 
 		if (choiceButton == 3) cout << "-> Exit" << endl;
 		else cout << " Exit" << endl;
@@ -243,164 +254,236 @@ int menuWrite()
 	}
 }
 
-void directorF(int& ageD, int& zpD, string& nameD, string& surnameD, string& firmaD, string& gender, string& educ)
-{
-	cout << "Enter Director's surname: ";
-	surnameD = writeWords();
-	cout << "\nEnter Director's name: ";
-	nameD = writeWords();
-	cout << "\nEnter Director's age: ";
-	ageD = writeNumbers();
-	cout << "\nEnter Director's company: ";
-	firmaD = writeWords();
-	cout << "\nEnter Director's salary: ";
-	zpD = writeNumbers();
-	cout << "\nEnter Director's gender: ";
-	gender = writeWords();
-	cout << "\nEnter Director's education: ";
-	educ = writeWords();
-	system("cls");
-};
+int menuWhoRed(Human*& mas, int n) {
+	int choose_menu = 0, keyboard_button = 0;
+	while (true) {
+		choose_menu = (choose_menu + (n + 1)) % (n + 1);
 
-void managerF(int& ageM, int& zpM, string& nameM, string& surnameM, string& firmaM, int& expirience, string& type)
-{
-	cout << "Enter Manager's surname: ";
-	surnameM = writeWords();
-	cout << "\nEnter Manager's name: ";
-	nameM = writeWords();
-	cout << "\nEnter Manager's age: ";
-	ageM = writeNumbers();
-	cout << "\nEnter Manager's company: ";
-	firmaM = writeWords();
-	cout << "\nEnter Manager's salary: ";
-	zpM = writeNumbers();
-	cout << "\nEnter Manager's expirience (how many years): ";
-	expirience = writeNumbers();
-	cout << "\nEnter Manager's type (junior, middle, higher): ";
-	type = writeWords();
-	system("cls");
-};
+		for (int i = 0; i < n; i++)
+			if (i == choose_menu) { cout << " -> " << i + 1 << " - " << mas[i].getSurname() << endl; }
+			else { cout << " " << i + 1 << " - " << mas[i].getSurname() << endl; }
 
-void programmerF(int& ageP, int& zpP, string& nameP, string& surnameP, string& firmaP, string& kind, string& roleP, string& gender, string& educ, int& expirience, string& type)
-{
-	cout << "Enter Programmer's surname: ";
-	surnameP = writeWords();
-	cout << "\nEnter Programmer's name: ";
-	nameP = writeWords();
-	cout << "\nEnter Programmer's age: ";
-	ageP = writeNumbers();
-	cout << "\nEnter Programmer's company: ";
-	firmaP = writeWords();
-	cout << "\nEnter Programmer's salary: ";
-	zpP = writeNumbers();
-	cout << "\nEnter Programmer's kind (tester or developer): ";
-	kind = writeWords();
-	cout << "\nEnter Programmer's role (application, web, systemic): ";
-	roleP = writeWords();
-	cout << "\nEnter Programmer's gender: ";
-	gender = writeWords();
-	cout << "\nEnter Programmer's education: ";
-	educ = writeWords();
-	cout << "\nEnter Programmer's expirience (how many years): ";
-	expirience = writeNumbers();
-	cout << "\nEnter Programmer's type (junior, middle, higher): ";
-	type = writeWords();
-	system("cls");
-};
+		if (choose_menu == n) { cout << " -> Exit from this menu" << endl; }
+		else cout << " Exit from this menu" << endl;
+
+		keyboard_button = _getch();
+		if (keyboard_button == 224) {
+			keyboard_button = _getch();
+			if (keyboard_button == 72) choose_menu--;
+			if (keyboard_button == 80) choose_menu++;
+		}
+		if (keyboard_button == 13) { system("cls"); return choose_menu; }
+		system("cls");
+	}
+}
+
+int menuWhatRed(Human*& mas, int choose) {
+	int choose_menu = 0, keyboard_button = 0;
+	while (true) {
+		choose_menu = (choose_menu + 10) % 10;
+
+		if (choose_menu == 0) { cout << " -> Surname: " << mas[choose].getSurname() << endl; }
+		else cout << " Surname: " << mas[choose].getSurname() << endl;
+
+		if (choose_menu == 1) { cout << " -> Name: " << mas[choose].getName() << endl; }
+		else cout << " Name: " << mas[choose].getName() << endl;
+
+		if (choose_menu == 2) { cout << " -> Company: " << mas[choose].getCompany() << endl; }
+		else cout << " Company: " << mas[choose].getCompany() << endl;
+
+		if (choose_menu == 3) { cout << " -> Position: " << mas[choose].getPosition() << endl; }
+		else cout << " Position: " << mas[choose].getPosition() << endl;
+
+		if (choose_menu == 4) { cout << " -> Age: " << mas[choose].getAge() << endl; }
+		else cout << " Age: " << mas[choose].getAge() << endl;
+
+		if (choose_menu == 5) { cout << " -> Expirience: " << mas[choose].getExpirience() << endl; }
+		else cout << " Expirience: " << mas[choose].getExpirience() << endl;
+
+		if (choose_menu == 6) { cout << " -> Gender: " << mas[choose].getGender() << endl; }
+		else cout << " Gender: " << mas[choose].getGender() << endl;
+
+		if (choose_menu == 7) { cout << " -> Education: " << mas[choose].getEducation() << endl; }
+		else cout << " Education: " << mas[choose].getEducation() << endl;
+
+		if (choose_menu == 8) { cout << " -> Status: " << mas[choose].getStatus() << endl; }
+		else cout << " Status: " << mas[choose].getStatus() << endl;
+
+		if (choose_menu == 9) { cout << " -> Exit from this menu " << endl; }
+		else cout << " Exit from this menu " << endl;
+
+
+		keyboard_button = _getch();
+		if (keyboard_button == 224) {
+			keyboard_button = _getch();
+			if (keyboard_button == 72) choose_menu--;
+			if (keyboard_button == 80) choose_menu++;
+		}
+		if (keyboard_button == 13) { system("cls"); return choose_menu; }
+		system("cls");
+	}
+}
 
 int main()
 {
-	string surnameP, nameP, nameD, nameM, surnameD, surnameM, firmaD, firmaM ,firmaP, roleP, kind, type, gender, educ;
-	int ageP, zpP, ageD, zpD, ageM, zpM, stazh;
-	int n = 1;
-	Administrirovanie** mas = new Administrirovanie * [n];
-	n--;
+	int n = 0;
+	Human* mas = new Human [n];
 	while (true)
 	{
 		int choice = menuBegin();
 		if (choice == 0)
 		{
-			while (true)
-			{
-				int choiceWrite = menuWrite();
-				if (choiceWrite == 0)
-				{
-					directorF(ageD, zpD, nameD, surnameD, firmaD, gender, educ);
-					mas[n] = new Director(ageD, zpD, nameD, surnameD, firmaD, gender, educ);
-					mas[n]->show();
-					mas[n]->Pension(ageD, gender);
-					n++;
-					n++;
-					Administrirovanie** masD = new Administrirovanie * [n];
-					n--;
-					copy(mas, mas + n, masD);
-					mas = masD;
-					masD = NULL;
-					delete[]masD;
+			Human sum;
+			cout << "Surname: ";
+			string ptr1 = writeWords();
+			sum.changeSurname(ptr1);
+			system("cls");
+			cout << "Name: ";
+			ptr1 = writeWords();
+			sum.changeName(ptr1);
+			system("cls");
+			cout << "Age: ";
+			int ptr = writeNumbers();
+			sum.changeAge(ptr);
+			system("cls");
+			cout << "Company: ";
+			ptr1 = writeWords();
+			sum.changeCompany(ptr1);
+			system("cls");
+			cout << "Position: ";
+			ptr1 = writeWords();
+			sum.changePosition(ptr1);
+			system("cls");
+			cout << "Expirience: ";
+			ptr = writeNumbers();
+			sum.changeExpirience(ptr);
+			system("cls");
+			cout << "Gender: ";
+			ptr1 = writeWords();
+			sum.changeGender(ptr1);
+			system("cls");
+			cout << "Education: ";
+			ptr1 = writeWords();
+			sum.changeEducation(ptr1);
+			system("cls");
+			cout << "Status: ";
+			ptr1 = writeWords();
+			sum.changeStatus(ptr1);
+			system("cls");
+			n++;
+			Human* ptrr = new Human[n];
+			for (int i = 0; i < n - 1; i++)
+				ptrr[i] = mas[i];
+			mas = ptrr;
+			ptrr = NULL;
+			delete[]ptrr;
+			mas[n - 1] = sum;
+		}
+		else
+			if (choice == 1) {
+				if (n != 0) {
+					while (true) {
+						int choose1 = menuWhoRed(mas, n);
+						if (choose1 == n) break;
+						while (true) {
+							int choose2 = menuWhatRed(mas, choose1);
+							if (choose2 == 9) break;
+							if (choose2 == 0) {
+								cout << "Enter surname ";
+								string ptr = writeWords();
+								mas[choose1].changeSurname(ptr);
+								system("cls");
+							}
+							else
+								if (choose2 == 1) {
+									cout << "Enter name: ";
+									string ptr = writeWords();
+									mas[choose1].changeName(ptr);
+									system("cls");
+								}
+								else
+									if (choose2 == 2) {
+										cout << "Enter company: ";
+										string ptr = writeWords();
+										mas[choose1].changeCompany(ptr);
+										system("cls");
+									}
+									else
+										if (choose2 == 3) {
+											cout << "Enter position: ";
+											string ptr = writeWords();
+											mas[choose1].changePosition(ptr);
+											system("cls");
+										}
+										else
+											if (choose2 == 4) {
+												cout << "Enter age: ";
+												int ptr = writeNumbers();
+												mas[choose1].changeAge(ptr);
+												system("cls");
+											}
+											else
+												if (choose2 == 5) {
+													cout << "Enter expirience: ";
+													int ptr = writeNumbers();
+													mas[choose1].changeExpirience(ptr);
+													system("cls");
+												}
+												else
+													if (choose2 == 6) {
+														cout << "Enter gender: ";
+														string ptr = writeWords();
+														mas[choose1].changeGender(ptr);
+														system("cls");
+													}
+													else
+														if (choose2 == 7) {
+															cout << "Enter education: ";
+															string ptr = writeWords();
+															mas[choose1].changeEducation(ptr);
+															system("cls");
+														}
+														else
+															if (choose2 == 6) {
+																cout << "Enter status: ";
+																string ptr = writeWords();
+																mas[choose1].changeStatus(ptr);
+																system("cls");
+															}
+						}
+					}
+				}
+				else {
+					cout << "You don't add humans yet" << endl;
 					system("pause");
 					system("cls");
 				}
-				else
-					if (choiceWrite == 1)
+			}
+			else 
+				if (choice == 2)
+				{
+					if (n != 0)
 					{
-						managerF(ageM, zpM, nameM, surnameM, firmaM, stazh, type);
-						mas[n] = new productManager(ageM, zpM, nameM, surnameM, firmaM, stazh, type);
-						mas[n]->show();
-						mas[n]->Pension(ageM, gender);
-						n++;
-						n++;
-						Administrirovanie** masM = new Administrirovanie * [n];
-						n--;
-						copy(mas, mas + n, masM);
-						mas = masM;
-						masM = NULL;
-						delete[]masM;
+						for (int i = 0; i < n; i++) {
+							mas[i].showHumanInfo();
+							cout << endl;
+							cout << endl;
+						}
 						system("pause");
 						system("cls");
 					}
 					else
-						if (choiceWrite == 2)
-						{
-							programmerF(ageP, zpP, nameP, surnameP, firmaP, kind, roleP, gender, educ, stazh, type);
-							mas[n] = new Programmer(ageP, zpP, nameP, surnameP, firmaP, kind, roleP, gender, educ, stazh, type);
-							mas[n]->show();
-							mas[n]->Pension(ageP, gender);
-							n++;
-							n++;
-							Administrirovanie** masP = new Administrirovanie * [n];
-							n--;
-							copy(mas, mas + n, masP);
-							mas = masP;
-							masP = NULL;
-							delete[]masP;
-							system("pause");
-							system("cls");
-						}
-						else break;
-			}
-		}
-		else
-			if (choice == 1)
-			{
-				if (n != 0)
-				{
-					for (int i = 0; i < n; i++)
-						mas[i]->show();
-					system("pause");
+					{
+						cout << "You don't add humans yet" << endl;
+						system("pause");
+						system("cls");
+					}
 				}
 				else
 				{
-					cout << "No information\n" << endl;
-					system("pause");
-					continue;
+					cout << "(0^0) Goodbye!!! (0^0)" << endl;
+					break;
 				}
-			}
-			else
-			{
-				cout << "(0^0) Goodbye!!! (0^0)" << endl;
-				break;
-			}
 	}
-	delete[]mas;
 	return 0;
 }
